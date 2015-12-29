5d63cd8723ceb32c1cefb26a9a67e5be0a9fd37e
lecion <ylc931116@gmail.com>
2015-12-29 23:28:35 +0800

    add butterknife

--- app/src/main/java/com/yliec/ewu/BaseActivity.java
+++ app/src/main/java/com/yliec/ewu/BaseActivity.java
@@ -1,10 +1,29 @@
 package com.yliec.ewu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import nucleus.presenter.Presenter;
+import nucleus.view.NucleusAppCompatActivity;
 
 /**
  * Created by Lecion on 11/18/15.
  */
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity<PresenterType extends Presenter> extends NucleusAppCompatActivity<PresenterType> {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        injectPrensenter();
+        super.onCreate(savedInstanceState);
+        if (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT != getRequestedOrientation()) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        setContentView(getContentView());
+
+    }
+
+    protected abstract int getContentView();
+
+    protected abstract void injectPrensenter();
+
 
 }
