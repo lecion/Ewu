b523ead7ed620696928f12afafdc41d3b2d74c7d
lecion <ylc931116@gmail.com>
2015-12-30 00:16:35 +0800

    封装BaseActivity

--- app/src/main/java/com/yliec/ewu/BaseActivity.java
+++ app/src/main/java/com/yliec/ewu/BaseActivity.java
@@ -2,7 +2,14 @@ package com.yliec.ewu;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import nucleus.presenter.Presenter;
 import nucleus.view.NucleusAppCompatActivity;
 
@@ -10,6 +17,15 @@ import nucleus.view.NucleusAppCompatActivity;
  * Created by Lecion on 11/18/15.
  */
 public abstract class BaseActivity<PresenterType extends Presenter> extends NucleusAppCompatActivity<PresenterType> {
+
+    @Nullable
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @Nullable
+    @Bind(R.id.toolbar_title)
+    TextView mToolbarTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         injectPrensenter();
@@ -18,7 +34,28 @@ public abstract class BaseActivity<PresenterType extends Presenter> extends Nucl
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         }
         setContentView(getContentView());
+        initToolbar();
+    }
 
+    protected void initToolbar() {
+        if (mToolbar == null) {
+            return;
+        }
+        setSupportActionBar(mToolbar);
+        if (mToolbarTitle != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        if (!TextUtils.isEmpty(NavUtils.getParentActivityName(this))) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_back);
+            mToolbar.setNavigationOnClickListener(view -> finish());
+        }
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        ButterKnife.bind(this);
     }
 
     protected abstract int getContentView();
