d6b1b0c09db2c3aa6c624ebc3ab703bb755aaf89
lecion <ylc931116@gmail.com>
2016-04-17 01:34:35 +0800

    以 RxPresenter 重构 AlbumPrensenter

--- app/src/main/java/com/yliec/ewu/module/publish/AlbumPresenter.java
+++ app/src/main/java/com/yliec/ewu/module/publish/AlbumPresenter.java
@@ -2,12 +2,9 @@ package com.yliec.ewu.module.publish;
 
 import android.os.Bundle;
 
-import com.yliec.ewu.api.entity.element.ImageBucket;
 import com.yliec.ewu.app.base.BasePresenter;
 import com.yliec.ewu.model.LocalImageModel;
 import com.yliec.lsword.compat.util.L;
-
-import java.util.HashMap;
 
 import javax.inject.Inject;
 
@@ -17,28 +14,24 @@ import rx.android.schedulers.AndroidSchedulers;
  * Created by Lecion on 4/14/16.
  */
 public class AlbumPresenter extends BasePresenter<AlbumActivity> {
+    public static final int REQUEST_IMAGES_ID = 1;
     private final String TAG = getClass().getSimpleName();
     @Inject
     LocalImageModel mLocalImageModel;
 
-    private HashMap<String, ImageBucket> bucketList = new HashMap<>();
-    private HashMap<String, String> thumbnailList = new HashMap<>();
-    private AlbumActivity mAlbumActivity = null;
-
     @Override
     protected void onCreate(Bundle savedState) {
         super.onCreate(savedState);
+        restartableLatestCache(REQUEST_IMAGES_ID,
+                () -> mLocalImageModel.getLocalImages().observeOn(AndroidSchedulers.mainThread()),
+                AlbumActivity::onItemChange,
+                AlbumActivity::onNetError
+        );
     }
 
     public void loadPhotos() {
         long startTime = System.currentTimeMillis();
-        mLocalImageModel.getLocalImages()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(imageList -> {
-                    mAlbumActivity.onItemChange(imageList);
-                }, throwable -> {
-                    mAlbumActivity.onNetError(throwable);
-                });
+        start(REQUEST_IMAGES_ID);
         long endTime = System.currentTimeMillis();
         L.d(TAG, "use time: " + (endTime - startTime) + " ms");
     }
@@ -46,8 +39,7 @@ public class AlbumPresenter extends BasePresenter<AlbumActivity> {
     @Override
     protected void onTakeView(AlbumActivity albumActivity) {
         super.onTakeView(albumActivity);
-        mAlbumActivity = albumActivity;
-        mAlbumActivity.injectModle(mLocalImageModel);
+        albumActivity.injectModle(mLocalImageModel);
     }
 
 }
