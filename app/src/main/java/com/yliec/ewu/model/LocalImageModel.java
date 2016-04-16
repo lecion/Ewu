defa4ca16cd629fc592ef922c0ee05dfc90c576c
lecion <ylc931116@gmail.com>
2016-04-17 01:23:06 +0800

    以 RxJava 重构 LocalImageModel

--- app/src/main/java/com/yliec/ewu/model/LocalImageModel.java
+++ app/src/main/java/com/yliec/ewu/model/LocalImageModel.java
@@ -28,7 +28,7 @@ public class LocalImageModel {
     ContentResolver mResolver;
 
     private HashMap<String, ImageBucket> bucketList = new HashMap<>();
-    private List<LocalImage> mLocalImages;
+    private List<LocalImage> mLocalImages = new ArrayList<>();
     private final String TAG = getClass().getSimpleName();
 
     public Observable<List<LocalImage>> getLocalImages() {
@@ -41,8 +41,6 @@ public class LocalImageModel {
                 Media.SIZE,
                 Media.BUCKET_DISPLAY_NAME
         };
-        L.d(TAG, "mLocalImages: " + mLocalImages);
-        mLocalImages = new ArrayList<>();
         return Observable.create(new Observable.OnSubscribe<Cursor>() {
             @Override
             public void call(Subscriber<? super Cursor> subscriber) {
@@ -54,55 +52,60 @@ public class LocalImageModel {
                 subscriber.onCompleted();
             }
         }).subscribeOn(Schedulers.io()).map(cursor -> {
-            if (cursor.moveToFirst()) {
-                int photoIDIndex = cursor.getColumnIndexOrThrow(Media._ID);
-                int photoPathIndex = cursor.getColumnIndexOrThrow(Media.DATA);
-                int photoNameIndex = cursor.getColumnIndexOrThrow(Media.DISPLAY_NAME);
-                int photoTitleIndex = cursor.getColumnIndexOrThrow(Media.TITLE);
-                int photoSizeIndex = cursor.getColumnIndexOrThrow(Media.SIZE);
-                int bucketDisplayNameIndex = cursor.getColumnIndexOrThrow(Media.BUCKET_DISPLAY_NAME);
-                int bucketIdIndex = cursor.getColumnIndexOrThrow(Media.BUCKET_ID);
+            getImagesFromCursor(cursor);
+            L.d(TAG, "mLocalImages: " + mLocalImages);
+            return mLocalImages;
+        });
+    }
 
-                do {
-                    String _id = cursor.getString(photoIDIndex);
-                    String name = cursor.getString(photoNameIndex);
-                    String path = cursor.getString(photoPathIndex);
-                    String title = cursor.getString(photoTitleIndex);
-                    String size = cursor.getString(photoSizeIndex);
-                    String bucketName = cursor.getString(bucketDisplayNameIndex);
-                    String bucketId = cursor.getString(bucketIdIndex);
+    private void getImagesFromCursor(Cursor cursor) {
+        if (cursor.moveToFirst()) {
+            int photoIDIndex = cursor.getColumnIndexOrThrow(Media._ID);
+            int photoPathIndex = cursor.getColumnIndexOrThrow(Media.DATA);
+            int photoNameIndex = cursor.getColumnIndexOrThrow(Media.DISPLAY_NAME);
+            int photoTitleIndex = cursor.getColumnIndexOrThrow(Media.TITLE);
+            int photoSizeIndex = cursor.getColumnIndexOrThrow(Media.SIZE);
+            int bucketDisplayNameIndex = cursor.getColumnIndexOrThrow(Media.BUCKET_DISPLAY_NAME);
+            int bucketIdIndex = cursor.getColumnIndexOrThrow(Media.BUCKET_ID);
+
+            do {
+                String _id = cursor.getString(photoIDIndex);
+                String name = cursor.getString(photoNameIndex);
+                String path = cursor.getString(photoPathIndex);
+                String title = cursor.getString(photoTitleIndex);
+                String size = cursor.getString(photoSizeIndex);
+                String bucketName = cursor.getString(bucketDisplayNameIndex);
+                String bucketId = cursor.getString(bucketIdIndex);
 
 //                L.d(_id + " bucket: "
 //                        + bucketName + ", bucketId: " + bucketId + " name:" + name + " path:" + path
 //                        + " title: " + title + " size: " + size);
-                    ImageBucket bucket = bucketList.get(bucketId);
-                    if (bucket == null) {
-                        bucket = new ImageBucket();
-                        bucketList.put(bucketId, bucket);
-                        bucket.imageList = new ArrayList<LocalImage>();
-                        bucket.bucketName = bucketName;
-                    }
-                    bucket.count++;
-                    LocalImage imageItem = new LocalImage();
-                    imageItem.imageId = _id;
-                    imageItem.imagePath = path;
-                    bucket.imageList.add(imageItem);
+                ImageBucket bucket = bucketList.get(bucketId);
+                if (bucket == null) {
+                    bucket = new ImageBucket();
+                    bucketList.put(bucketId, bucket);
+                    bucket.imageList = new ArrayList<LocalImage>();
+                    bucket.bucketName = bucketName;
+                }
+                bucket.count++;
+                LocalImage imageItem = new LocalImage();
+                imageItem.imageId = _id;
+                imageItem.imagePath = path;
+                bucket.imageList.add(imageItem);
 
-                } while (cursor.moveToNext());
+            } while (cursor.moveToNext());
 
-                Iterator<Map.Entry<String, ImageBucket>> it = bucketList.entrySet().iterator();
+            Iterator<Map.Entry<String, ImageBucket>> it = bucketList.entrySet().iterator();
 
-                while (it.hasNext()) {
-                    Map.Entry<String, ImageBucket> entry = it.next();
-                    ImageBucket bucket = entry.getValue();
-                    for (int i = 0; i < bucket.imageList.size(); i++) {
-                        LocalImage image = bucket.imageList.get(i);
-                        mLocalImages.add(image);
-                    }
+            while (it.hasNext()) {
+                Map.Entry<String, ImageBucket> entry = it.next();
+                ImageBucket bucket = entry.getValue();
+                for (int i = 0; i < bucket.imageList.size(); i++) {
+                    LocalImage image = bucket.imageList.get(i);
+                    mLocalImages.add(image);
                 }
             }
-            return mLocalImages;
-        });
+        }
     }
 
 }
