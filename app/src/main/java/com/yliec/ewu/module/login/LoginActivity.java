cd6102452d0d0ab2378dd7b3e5405b0e638a952f
lecion <ylc931116@gmail.com>
2016-04-23 22:50:48 +0800

    完善 LoginActivity UI

--- app/src/main/java/com/yliec/ewu/module/login/LoginActivity.java
+++ app/src/main/java/com/yliec/ewu/module/login/LoginActivity.java
@@ -4,16 +4,38 @@ import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.yliec.ewu.R;
 import com.yliec.ewu.app.base.BaseActivity;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
+import butterknife.OnClick;
 import nucleus.factory.RequiresPresenter;
 
 @RequiresPresenter(LoginPresenter.class)
 public class LoginActivity extends BaseActivity<LoginPresenter> {
+
+    @Bind(R.id.btn_register)
+    Button mRegister;
+
+    @Bind(R.id.btn_login)
+    Button mLogin;
+
+    @Bind(R.id.et_username)
+    EditText mEtUsername;
+
+    @Bind(R.id.et_password)
+    EditText mEtPassword;
+
+    @Bind(R.id.pb_login)
+    ProgressBar mPbLogin;
 
     @Inject
     AccountManager mAccountManager;
@@ -38,4 +60,38 @@ public class LoginActivity extends BaseActivity<LoginPresenter> {
         return new Intent(context, LoginActivity.class);
     }
 
+    @OnClick(R.id.btn_register)
+    public void openRegister() {
+        Intent i = RegisterActivity.getCallingIntent(this);
+        startActivity(i);
+        finish();
+    }
+
+    @OnClick(R.id.btn_login)
+    public void doLogin() {
+        boolean validate = true;
+        String username = mEtUsername.getText().toString().trim();
+        String password = mEtPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(username)) {
+            mEtUsername.setError("用户名不能为空");
+            validate = false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            mEtPassword.setError("密码不能为空");
+            validate = false;
+        }
+        if (validate) {
+            showLoginProgress();
+            getPresenter().login(username, password);
+        }
+    }
+
+    public void showLoginProgress() {
+        mPbLogin.setVisibility(View.VISIBLE);
+    }
+
+    public void hideLoginProgress() {
+        mPbLogin.setVisibility(View.INVISIBLE);
+    }
 }
--- app/src/main/java/com/yliec/ewu/module/login/LoginPresenter.java
+++ app/src/main/java/com/yliec/ewu/module/login/LoginPresenter.java
@@ -1,12 +1,26 @@
 package com.yliec.ewu.module.login;
 
+import android.os.Handler;
+
 import nucleus.presenter.RxPresenter;
 
 /**
  * Created by Lecion on 4/11/16.
  */
 public class LoginPresenter extends RxPresenter<LoginActivity> {
+
+
     private String TAG = getClass().getSimpleName();
 
+    private Handler mHandler = new Handler();
 
+    public void login(String username, String password) {
+        getView().showLoginProgress();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getView().hideLoginProgress();
+            }
+        }, 3000);
+    }
 }
